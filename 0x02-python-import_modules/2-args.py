9dbb33101bde68822cad8854f202b1297c835cf0
Gloireski <belemgloirebekoutou8@gmail.com>
2023-03-09 14:18:35 -0800

    task3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if t < 2:
         print("{:d} arguments.".format(t - 1))
     elif t == 2:
-        print("{:d} arguments:".format(t - 1))
+        print("{:d} argument:".format(t - 1))
         print("{:d}: {:s}".format(t - 1, sys.argv[t - 1]))
     else:
         print("{:d} arguments:".format(t - 1))
