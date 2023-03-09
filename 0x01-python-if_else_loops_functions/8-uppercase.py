ec9bb4f2f40cda1bafd3cb51c90f00634943f747
Gloireski <belemgloirebekoutou8@gmail.com>
2023-03-08 16:22:08 -0800

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in range(len(str)):
+        if ord(str[c]) >= ord('a') and ord(str[c]) <= ord('z'):
+            asc = ord(str[c]) - 32
+        else:
+            asc = ord(str[c])
+        if c == len(str)-1:
+            print(chr(asc))
+        else:
+            print(chr(asc), end='')
