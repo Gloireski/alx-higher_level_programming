a6f2051a2185e500234e900aaaaf4cb7da45dd86
Gloireski <belemgloirebekoutou8@gmail.com>
2023-03-14 16:20:05 -0700

    task11

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 
 def divisible_by_2(my_list=[]):
     new_list = my_list[:]
-    for i in range(len(new_list)):
+    for i in range(0, len(new_list)):
         if (new_list[i] % 2) == 0:
             new_list[i] = True
         else:
