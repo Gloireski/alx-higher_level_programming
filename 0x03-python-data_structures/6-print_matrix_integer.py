0417b814acc23673bd8f6994dd15c04ff48a48a7
Gloireski <belemgloirebekoutou8@gmail.com>
2023-03-13 18:38:42 -0700

    task7

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        print('')
+    else:
+        for row in matrix:
+            for i in range(0, len(row)):
+                if i == len(row) - 1:
+                    print("{}".format(row[i]), end='')
+                else:
+                    print("{}".format(row[i]), end=' ')
+            print('')
