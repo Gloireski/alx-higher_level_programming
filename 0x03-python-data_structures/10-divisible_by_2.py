2d1a95ca9faeed4d7c4abb611364e019b3afb0d3
Gloireski <belemgloirebekoutou8@gmail.com>
2023-03-13 19:57:57 -0700

    task11

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
-def divisible_by_2(my_list=[]):
+def divisible_by_2(my<F5>_list=[]):
     new_list = []
     for i in range(0, len(my_list)):
         if (my_list[i] % 2) == 0:
             new_list.append(True)
         else:
             new_list.append(False)
-    return new_list
+    return new_list        
