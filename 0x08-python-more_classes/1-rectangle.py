33425ed5f01e51268cd282900b9fc8d2a415bf04
Gloireski <belemgloirebekoutou8@gmail.com>
2023-04-06 08:27:10 -0700

    task1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle(object):
     """complete rectangle"""
     def __init__(self, width=0, height=0):
         """ constructeur """
-        self.__check_valid_height(height)
-        self.__check_valid_width(width)
+        #self.__check_valid_height(height)
+        #self.__check_valid_width(width)
         self.height = height
         self.width = width
 
