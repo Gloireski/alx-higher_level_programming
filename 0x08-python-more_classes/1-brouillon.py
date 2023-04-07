9c7807aac2ffed329952658eae72a0f983b2b286
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-04-07 02:01:59 +0000

    Update 1-brouillon.py

--- 0x08-python-more_classes/1-brouillon.py
+++ 0x08-python-more_classes/1-brouillon.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # 1-rectangle.py
-# Carlos Barros <1543@holbertonschool.com>
 """ File name : 1-rectangle.py
     Real definition of a rectangle : class that defines a rectangle
     It is not allowed to import any module
@@ -8,19 +7,18 @@
 
 
 class Rectangle(object):
-    """Rectangle: Define new class"""
+    """complete rectangle"""
     def __init__(self, width=0, height=0):
-        """Initialize new class rectangle
-        Args:
-            width (int): width for the new rectangle
-            height (int): height for the new rectangle
+        """ constructeur
+            self.__check_valid_height(height)
+            self.__check_valid_width(width)
         """
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
-        """Property for attribute width."""
+        """returns the porperty width"""
         return self.__width
 
     @width.setter
@@ -37,17 +35,31 @@ class Rectangle(object):
 
     @property
     def height(self):
-        """Property for attribute height."""
-        return self.__height
+        """returns the porperty height"""
+        return self.___height
 
     @height.setter
     def height(self, value):
-        """Set values to height
+        """Set values to width
         Args:
-            value (int): new value for height
+            value (int): new value for width
         """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def __check_valid_width(self, width):
+        """ checker """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("widht must be >= 0")
+
+    def __check_valid_height(self, height):
+        """ height checker """
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 2")
