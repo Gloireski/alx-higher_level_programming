2b0b04de822c937a3a3ff503212343affac04288
Gloireski <belemgloirebekoutou8@gmail.com>
2023-04-12 11:16:03 -0700

    task5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+"""
+    File name : 5-base_geometry.py
+    Requirement: not allowed to import any module
+"""
+
+
+class BaseGeometry(object):
+    """
+        class BaseGeometry
+    """
+    pass
