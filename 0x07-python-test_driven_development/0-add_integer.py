0690806f827efd0bcdc26dba06c364d7b8c2091a
Gloireski <belemgloirebekoutou8@gmail.com>
2023-04-14 08:22:14 -0700

    task0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 0-add_integer.py
+"""
+    not allowed to import any module
+"""
+
+
+def add_integer(a, b=98):
+    """
+        sum a and b
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a + b)
