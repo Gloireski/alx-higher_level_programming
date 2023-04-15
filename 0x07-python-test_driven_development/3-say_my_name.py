56f2ed1e6a2208ad0bbbd5f8936397e886ac7ff5
Gloireski <belemgloirebekoutou8@gmail.com>
2023-04-14 18:17:32 -0700

    task3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,11 +2,12 @@
 # 2-matrix_divided.py
 """
     File name: 3-say_my_name.py
-    Say my name: Function that prints 
+    Say my name: Function that prints
     My name is <first name> <last name>
     Prototype: def say_my_name(first_name, last_name="")
     You are not allowed to import any module
 """
+
 
 def say_my_name(first_name, last_name=""):
     """
@@ -17,7 +18,6 @@ def say_my_name(first_name, last_name=""):
     """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print("My name is {:s} {:s}".format(first_name, last_name))
