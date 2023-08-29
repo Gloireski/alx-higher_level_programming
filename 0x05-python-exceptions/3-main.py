1b3e333ff5e81081b7592a1aafc190d89d6cf1f1
Gloireski <belemgloirebekoutou8@gmail.com>
2023-08-28 19:27:40 -0700

    task4

--- 0x05-python-exceptions/3-main.py
+++ 0x05-python-exceptions/3-main.py
@@ -6,3 +6,7 @@ b = 2
 result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result))
 
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
