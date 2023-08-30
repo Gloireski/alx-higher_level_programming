3a8f53d517036e8409047cc66051465c8b359325
Gloireski <belemgloirebekoutou8@gmail.com>
2023-08-30 14:06:36 -0700

    task5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+    first class
+"""
+
+
+class Square:
+    """define  class with size attr with validation"""
+    def __init__(self, size=0):
+        if type(size) is int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        for i in range(self.__size):
+            print("#" * self.__size)
