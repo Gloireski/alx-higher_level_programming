ba95871a742a6ff6d3f3dea8cb01174d93d4c2d5
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-08-31 17:28:39 +0100

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-for ascii in range(97, 123):
-    if ascii in [101, 113]:
-        continue
-    else:
-        print("{}".format(chr(ascii)), end='')
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-for nb in range(0, 99):
-    print("{} = {}".format(nb, hex(nb)))
