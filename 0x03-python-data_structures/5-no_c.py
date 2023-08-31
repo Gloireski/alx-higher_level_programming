218f6cae255e941e1600d51468da35540c31fb36
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-08-31 17:28:59 +0100

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def no_c(my_string):
-    list_str = ''
-    for el in my_string:
-        if el != 'c' and el != 'C':
-            list_str = list_str + el
-    list_str
-    return list_str
--- 0x03-python-data_structures/8-multiple_returns.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def multiple_returns(sentence):
-    if sentence == '':
-        t = len(sentence), "None"
-        return t
-    else:
-        t = len(sentence), sentence[0]
-        return t
