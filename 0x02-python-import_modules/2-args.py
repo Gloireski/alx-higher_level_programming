b33528ec19365c75604a8580ef05d2d674d84e37
Gloireski <belemgloirebekoutou8@gmail.com>
2023-10-05 16:39:20 +0100

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    t = len(sys.argv)
+    if t < 2:
+        print("{:d} arguments.".format(t - 1))
+    elif t == 2:
+        print("{:d} argument:".format(t - 1))
+        print("{:d}: {:s}".format(t - 1, sys.argv[t - 1]))
+    else:
+        print("{:d} arguments:".format(t - 1))
+        for c in range(1, t):
+            print("{:d}: {:s}".format(c, sys.argv[c]))
