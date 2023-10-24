59d2e0f778ebcdffb0802cf0071865a95a4a4393
Gloireski <belemgloirebekoutou8@gmail.com>
2023-10-24 16:21:38 +0100

    update

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+    first class
+"""
+
+
+class Square:
+    """define an empty class"""
+    {}
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+    first class
+"""
+
+
+class Square:
+    """define  class with size attr"""
+    def __init__(self, size):
+        self.__size = size
