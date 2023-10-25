341e6637a638079ef61a873d62aa9e540fa619c4
Gloireski <belemgloirebekoutou8@gmail.com>
2023-10-25 08:42:51 -0700

    fix bugs

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+# 3-safe_print_division.py
+# Belem Gloire BEKOUTOU 
 
 def safe_print_division(a, b):
+    """Returns the result of the divison of a by b"""
     try:
         result = a / b
     except ZeroDivisionError:
