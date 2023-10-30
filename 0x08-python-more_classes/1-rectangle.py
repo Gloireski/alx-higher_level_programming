ae21ffb147f4f01bf19c90a82c5e52f5d6b50c0c
Gloireski <belemgloirebekoutou8@gmail.com>
2023-10-30 12:08:27 -0700

    task1 fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -30,12 +30,11 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Sets value to width"""
-        if isinstance(value, int):
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
-        else:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @height.setter
     def height(self, value):
