509cc5f45389af26edd9924e6c66b5d588bc7f85
Gloireski <belemgloirebekoutou8@gmail.com>
2023-10-30 11:59:45 -0700

    task1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Belem Gloire BEKOUTOU
+"""Define a rectangle"""
+
+
+class Rectangle:
+    """Represents a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.__width = 0
+        self.__height = 0
+
+    @property
+    def width(self):
+        """Returns width value"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Returns height value"""
+        return self.height
+
+    @width.setter
+    def width(self, value):
+        """Sets value to width"""
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+        else:
+            raise TypeError("width must be an integer")
+
+    @height.setter
+    def height(self, value):
+        """Sets value to height"""
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
+        else:
+            raise TypeError("height must be an integer")
