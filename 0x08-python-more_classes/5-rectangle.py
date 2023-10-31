59c214c606e56d72199ec5ca340b938c996ab487
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-10-31 03:08:37 +0100

    Update 5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -27,6 +27,25 @@ class Rectangle:
         """Returns height value"""
         return self.__height
 
+    @width.setter
+    def width(self, value):
+        """Sets value to width"""
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+        else:
+            raise TypeError("width must be an integer")
+
+    @height.setter
+    def height(self, value):
+        """Sets value to height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
     def area(self):
         """Returns the rectangle area"""
         return (self.__width * self.__height)
@@ -59,22 +78,3 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
-
-    @width.setter
-    def width(self, value):
-        """Sets value to width"""
-        if type(value) is int:
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
-        else:
-            raise TypeError("width must be an integer")
-
-    @height.setter
-    def height(self, value):
-        """Sets value to height"""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
