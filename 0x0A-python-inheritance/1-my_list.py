ecb92ddab3438412b0b0b8c277a3966f904bdfbf
Gloireski <belemgloirebekoutou8@gmail.com>
2023-11-06 10:47:10 -0800

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-my_list.py
+# Belem Gloire BEKOUTOU
+"""Define MyList class"""
+
+
+class MyList(list):
+    """MyList class, inherits list"""
+    def print_sorted(self):
+        """prints the list, but sorted (asc sort)"""
+        print(sorted(self))
