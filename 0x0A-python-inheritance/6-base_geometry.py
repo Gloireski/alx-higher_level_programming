eee747e2f0b0932c5b676b05db769791f0dc8daf
Gloireski <belemgloirebekoutou8@gmail.com>
2023-11-07 04:12:57 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+# Belem Gloire BEKOUTOU
+"""define class base on BaseGeometry"""
+
+
+class BaseGeometry(object):
+    """BaseGeometry based class"""
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
