6cfb0b317848016a700e81ea8673d9fd53a07b4d
Gloireski <belemgloirebekoutou8@gmail.com>
2023-11-07 14:22:00 -0800

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 6-load_from_json_file.py
+# Belem Gloire BEKOUTOU
+"""define load_from_json_file"""
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a “JSON file”
+
+    Args:
+        filename: file from which to create object
+    """
+    with open(filename, mode="r") as f:
+        json.load(f)
