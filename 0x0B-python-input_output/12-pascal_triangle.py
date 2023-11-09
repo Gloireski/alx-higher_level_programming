04f15655fa5e899401ec41b87c99247c1e5c3465
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-11-09 13:11:56 +0100

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,11 +3,10 @@
 # Belem Gloire BEKOUTOU
 """Defines a Pascal's Triangle function."""
 
-if __name__ == "__main__":
-
-    def pascal_triangle(n):
-        list1 = []
-        for i in range(1, n + 1):
-            for j in range(1, i + 1):
-                list1.append(j)
-        return list1
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            list1.append(j)
+    return list1
