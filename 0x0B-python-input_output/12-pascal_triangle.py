ef770bc6d119339dd88a0c754f324c2a752521f1
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-11-09 13:16:58 +0100

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,9 +7,6 @@ def pascal_triangle(n):
 
     Returns a list of lists of integers representing the triangle.
     """
-    if n <= 0:
-        return []
-
     list1 = []
     for i in range(1, n + 1):
         for j in range(1, i + 1):
