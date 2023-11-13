d4a43066d93d0e088d211def5dad7da3f18ea205
Gloireski <belemgloirebekoutou8@gmail.com>
2023-11-13 11:14:34 -0800

    task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# square.py
+# Belem Gloire BEKOUTOU
+"""defines square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialize variables"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                self.y, self.width))
