348926ebaf2da0c60947ed9e1697d684af3f8673
Gloireski <belemgloirebekoutou8@gmail.com>
2023-11-13 13:53:42 -0800

    task 19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -64,3 +64,14 @@ class Base:
             new = cls(1)
         new.update(**dictionary)
         return new
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as jsf:
+                list_dict = Base.from_json_string(jsf.read())
+                return [cls.create(**d) for d in list_dict]
+        except IOError:
+            return []
