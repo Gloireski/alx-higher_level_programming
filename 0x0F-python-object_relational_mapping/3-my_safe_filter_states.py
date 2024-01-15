dbe2ebba6b5653dc0dca25d8592f9d4861908f9b
Gloireski <belemgloirebekoutou8@gmail.com>
2024-01-14 18:55:53 -0800

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# Belem Gloire BEKOUTOU
+# 3-my_safe_filter_states.py
+"""script that takes in an argument and displays all values in the states
+   table of hbtn_0e_0_usa where name matches the argument safe
+   from MySQL injections!.
+    Args:
+        mysql username;
+        mysql password;
+        database name;
+        state name searched
+    module: MySQLdb (import MySQLdb)
+    server: localhost at port 3306
+    Results must be sorted in ascending order by states.id
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         password=sys.argv[2], database=sys.argv[3], port=3306)
+    c = db.cursor()
+    match = sys.argv[4]
+    c.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
