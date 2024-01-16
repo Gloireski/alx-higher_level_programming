d13cce8fd13405045e4fbaa806035535354dc1b9
Gloireski <belemgloirebekoutou8@gmail.com>
2024-01-15 18:22:25 -0800

    task 14

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# Belem Gloire BEKOUTOU
+# model_city.py
+"""class definition of a State and an instance Base = declarative_base():
+   .inherits from Base
+   .links to the MySQL table cities
+   .class attribute id: col(auto gen, uniq int, not nul, pk)
+   .class attribute name: col(string, max 128, not null)
+   Must use sql alchemy
+   Server: localhost at port 3306
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import State
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """city model class"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+
+    def __init__(self, name):
+        "constructor"
+        self.name = name
