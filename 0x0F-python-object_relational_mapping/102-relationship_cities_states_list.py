32b7c0c410cc9a1e2687a9f8a128e57c0be8ecbd
Gloireski <belemgloirebekoutou8@gmail.com>
2024-01-16 09:34:01 -0800

    task 102

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -22,4 +22,4 @@ if __name__ == '__main__':
 
     for state in session.query(State).order_by(State.id):
         for city in state.cities:
-            print("{} -> {} -> {}".format(city.id, city.name, state.name))
+            print("{} : {} -> {}".format(city.id, city.name, state.name))
