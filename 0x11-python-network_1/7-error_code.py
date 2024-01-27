e5276375a87e1469d4ed655f4b6db1232a03de96
Gloireski <belemgloirebekoutou8@gmail.com>
2024-01-26 18:31:23 -0800

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,8 +10,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    try:
-        req = requests.get(url)
-        print(req.text)
-    except requests.exceptions.HTTPError as er:
-        print("Error code: {}".format(er.status_code))
+    req = requests.get(url)
+    status = req.status_code
+    print(req.text) if status < 400 else print(
+        "Error code: {}".format(req.status_code))
