f155e997c608e295c0e2da7be324fdcd03f329c0
Gloireski <belemgloirebekoutou8@gmail.com>
2024-01-26 18:59:20 -0800

    10-my_github.py, 100-github_commits.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     url = 'https://api.github.com/users/{}'.format(argv[1])
     r = requests.get(url,
                      auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(r.json().get('id'))
+    print(r.json().get("id"))
