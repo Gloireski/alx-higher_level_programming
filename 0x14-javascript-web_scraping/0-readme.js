19367c886b21cfaa29d51670cd4ce66ebf4e6ba4
Gloireski <belemgloirebekoutou8@gmail.com>
2024-02-20 16:06:33 -0800

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,5 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
-  if (data) {
-    console.log(data);
-  } else console.log(err);
+  console.log(err || data);
 });
